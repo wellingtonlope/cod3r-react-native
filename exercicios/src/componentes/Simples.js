80f403cbc17700fdda622386f6834939627bae1a
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-01-08 14:28:02 -0400

    Fundamentos: criando estilo

--- exercicios/src/componentes/Simples.js
+++ exercicios/src/componentes/Simples.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Text } from 'react-native'
+import Padrao from '../estilo/Padrao'
 
 // export default function (props) {
 //     return <Text>{props.texto}</Text>
 // }
 
 export default props =>
-    <Text>Arrow: {props.texto}</Text>
+    <Text style={Padrao.ex}>Arrow: {props.texto}</Text>
 
