c654cc3a658712f8fd696a7f8ffd446d9ca9114d
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-01-10 14:01:55 -0400

    Comunicação indireta via callback

--- exercicios/src/Menu.js
+++ exercicios/src/Menu.js
@@ -9,8 +9,13 @@ import Plataformas from './componentes/Plataformas'
 import ValidarProps from './componentes/ValidarProps'
 import Evento from './componentes/Evento'
 import { Avo } from './componentes/ComunicacaoDireta'
+import TextoSincronizado from './componentes/ComunicacaoIndireta'
 
 export default createDrawerNavigator({
+    TextoSincronizado: {
+        screen: TextoSincronizado,
+        navigationOptions: { title: 'Texto Sincronizado' }
+    },
     Avo: {
         screen: () => <Avo nome='João' sobrenome='Silva' />,
         // navigationOptions: { title: 'Comunicação Direta' }
