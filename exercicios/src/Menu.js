814a904264da6ecfe14d3968ed2c3a5db21e514e
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-01-10 15:33:16 -0400

    Fundamentos: Lista de Alunos

--- exercicios/src/Menu.js
+++ exercicios/src/Menu.js
@@ -10,8 +10,13 @@ import ValidarProps from './componentes/ValidarProps'
 import Evento from './componentes/Evento'
 import { Avo } from './componentes/ComunicacaoDireta'
 import TextoSincronizado from './componentes/ComunicacaoIndireta'
+import ListaFlex from './componentes/ListaFlex'
 
 export default createDrawerNavigator({
+    ListaFlex: {
+        screen: ListaFlex,
+        navigationOptions: { title: 'Lista (Flex Box)' }
+    },
     TextoSincronizado: {
         screen: TextoSincronizado,
         navigationOptions: { title: 'Texto Sincronizado' }
