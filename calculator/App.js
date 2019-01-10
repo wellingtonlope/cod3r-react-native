b745c0eaa4acc37fe07beb77d07fb0031b2c54e2
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-01-10 17:44:47 -0400

    Calculadora: Componente Botão

--- calculator/App.js
+++ calculator/App.js
@@ -1,49 +1,46 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react'
+import { Platform, StyleSheet, Text, View } from 'react-native'
+import Button from './src/components/Button'
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+export default class App extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.buttons}>
+                    <Button label='AC' />
+                    <Button label='/' />
+                    <Button label='7' />
+                    <Button label='8' />
+                    <Button label='9' />
+                    <Button label='*' />
+                    <Button label='4' />
+                    <Button label='5' />
+                    <Button label='6' />
+                    <Button label='-' />
+                    <Button label='1' />
+                    <Button label='2' />
+                    <Button label='3' />
+                    <Button label='+' />
+                    <Button label='0' />
+                    <Button label='.' />
+                    <Button label='=' />
+                </View>
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    container: {
+        flex: 1,
+    },
+    buttons: {
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+})
