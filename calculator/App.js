4cd2ab5c2323694f7c3be0f14e2f0d45edea1bb5
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-01-10 18:09:50 -0400

    Calculadora: Componente botão

--- calculator/App.js
+++ calculator/App.js
@@ -8,28 +8,40 @@ export default class App extends Component {
         displayValue: '0'
     }
 
+    addDigit = n => {
+        this.setState({ displayValue: n })
+    }
+
+    clearMemory = () => {
+        this.setState({ displayValue: 0 })
+    }
+
+    setOperation = operation => {
+
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <Display value={this.state.displayValue} />
                 <View style={styles.buttons}>
-                    <Button label='AC' />
-                    <Button label='/' />
-                    <Button label='7' />
-                    <Button label='8' />
-                    <Button label='9' />
-                    <Button label='*' />
-                    <Button label='4' />
-                    <Button label='5' />
-                    <Button label='6' />
-                    <Button label='-' />
-                    <Button label='1' />
-                    <Button label='2' />
-                    <Button label='3' />
-                    <Button label='+' />
-                    <Button label='0' />
-                    <Button label='.' />
-                    <Button label='=' />
+                    <Button label='AC' triple onPress={this.clearMemory} />
+                    <Button label='/' operation onPress={this.setOperation} />
+                    <Button label='7' onPress={this.addDigit} />
+                    <Button label='8' onPress={this.addDigit} />
+                    <Button label='9' onPress={this.addDigit} />
+                    <Button label='*' operation onPress={this.setOperation} />
+                    <Button label='4' onPress={this.addDigit} />
+                    <Button label='5' onPress={this.addDigit} />
+                    <Button label='6' onPress={this.addDigit} />
+                    <Button label='-' operation onPress={this.setOperation} />
+                    <Button label='1' onPress={this.addDigit} />
+                    <Button label='2' onPress={this.addDigit} />
+                    <Button label='3' onPress={this.addDigit} />
+                    <Button label='+' operation onPress={this.setOperation} />
+                    <Button label='0' double onPress={this.addDigit} />
+                    <Button label='.' onPress={this.addDigit} />
+                    <Button label='=' operation onPress={this.setOperation} />
                 </View>
             </View>
         )
