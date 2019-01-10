08e378ea5c98919f18d59d4ab86372304f802d7f
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-01-10 16:22:35 -0400

    Fundamentos: Exemplo Flex

--- exercicios/src/Menu.js
+++ exercicios/src/Menu.js
@@ -11,8 +11,12 @@ import Evento from './componentes/Evento'
 import { Avo } from './componentes/ComunicacaoDireta'
 import TextoSincronizado from './componentes/ComunicacaoIndireta'
 import ListaFlex from './componentes/ListaFlex'
+import Flex from './componentes/Flex'
 
 export default createDrawerNavigator({
+    Flex: {
+        screen: Flex
+    },
     ListaFlex: {
         screen: ListaFlex,
         navigationOptions: { title: 'Lista (Flex Box)' }
