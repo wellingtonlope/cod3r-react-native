e95412e1d5d1285fe924ecbeee401e176c873edc
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-01-11 15:27:12 -0400

    Mines: Field

--- mines/App.js
+++ mines/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import params from './src/params'
+import Field from './src/components/Field'
 
 export default class App extends Component {
     render() {
@@ -11,6 +12,13 @@ export default class App extends Component {
                     Tamanho da grade:
                     {params.getRowsAmount()}x{params.getColumnsAmount()}
                 </Text>
+                <Field />
+                <Field opened />
+                <Field opened nearMines={1} />
+                <Field opened nearMines={2} />
+                <Field opened nearMines={3} />
+                <Field opened nearMines={6} />
+
             </View>
         );
     }
