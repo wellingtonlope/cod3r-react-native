ce8c035a341a0dbc15554a0b79e2141c36bf1820
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-01-11 19:16:59 -0400

    Mines: Abrir Campo

--- mines/App.js
+++ mines/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 import params from './src/params'
 import MineField from './src/components/MineField'
-import { createMinedBoard } from './src/functions'
+import {
+    createMinedBoard,
+    cloneBoard,
+    openField,
+    hasExplosion,
+    wonGame,
+    showMines
+} from './src/functions'
 
 export default class App extends Component {
 
@@ -23,8 +30,28 @@ export default class App extends Component {
         const rows = params.getRowsAmount()
 
         return {
-            board: createMinedBoard(rows, cols, this.minesAmount())
+            board: createMinedBoard(rows, cols, this.minesAmount()),
+            won: false,
+            lost: false
         }
+    }
+
+    onOpenField = (row, column) => {
+        const board = cloneBoard(this.state.board)
+        openField(board, row, column)
+        const lost = hasExplosion(board)
+        const won = wonGame(board)
+
+        if (lost) {
+            showMines(board)
+            Alert.alert('Perdeeeeu!', 'Que buuuuro!')
+        }
+
+        if (won) {
+            Alert.alert('Parabéns', 'Você Venceu!')
+        }
+
+        this.setState({ board, lost, won })
     }
 
     render() {
@@ -36,7 +63,8 @@ export default class App extends Component {
                     {params.getRowsAmount()}x{params.getColumnsAmount()}
                 </Text>
                 <View style={styles.board}>
-                    <MineField board={this.state.board} />
+                    <MineField board={this.state.board}
+                        onOpenField={this.onOpenField} />
                 </View>
             </View>
         );
