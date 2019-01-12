d26f6871d875a85594ede3c3e7f3598dada5f9a0
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-01-12 17:34:56 -0400

    tasks: Input de autenticação

--- tasks/src/screens/Auth.js
+++ tasks/src/screens/Auth.js
@@ -8,6 +8,7 @@ import {
     TouchableOpacity,
     Alert,
 } from 'react-native'
+import AuthInput from '../components/AuthInput'
 import commonStyles from '../commonStyles'
 import backgroundImage from '../../assets/imgs/login.jpg'
 
@@ -39,18 +40,22 @@ export default class Auth extends Component {
                             'Crie a sua conta' : 'Informe seus dados'}
                     </Text>
                     {this.state.stageNew &&
-                        <TextInput placeholder='Nome' style={styles.input}
+                        <AuthInput icon='user' placeholder='Nome'
+                            style={styles.input}
                             value={this.state.name}
                             onChangeText={name => this.setState({ name })} />
                     }
-                    <TextInput placeholder='E-mail' style={styles.input}
+                    <AuthInput icon='at' placeholder='E-mail'
+                        style={styles.input}
                         value={this.state.email}
                         onChangeText={email => this.setState({ email })} />
-                    <TextInput placeholder='Senha' style={styles.input}
+                    <AuthInput icon='lock' secureTextEntry={true} placeholder='Senha'
+                        style={styles.input}
                         value={this.state.password}
                         onChangeText={password => this.setState({ password })} />
                     {this.state.stageNew &&
-                        <TextInput placeholder='Confirmação' style={styles.input}
+                        <AuthInput icon='asterisk' secureTextEntry={true} placeholder='Confirmação'
+                            style={styles.input}
                             value={this.state.confirmPassword}
                             onChangeText={confirmPassword => this.setState({ confirmPassword })} />
                     }
