75fe636bc34a80c5a55aa2a0cc6850cf94e86586
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-01-12 16:41:39 -0400

    tasks-backend: api de tasks

--- tasks-backend/config/routes.js
+++ tasks-backend/config/routes.js
@@ -2,8 +2,27 @@ module.exports = app => {
     const {
         user,
         auth,
+        task,
     } = app.api
+
+    const {
+        authenticate
+    } = app.config.passport
 
     app.post('/signup', user.save)
     app.post('/signin', auth.signin)
+
+    app.route('/tasks')
+        .all(authenticate())
+        .get(task.getTasks)
+        .post(task.save)
+
+    app.route('/tasks/:id')
+        .all(authenticate())
+        .delete(task.remove)
+
+    app.route('/tasks/:id/toggle')
+        .all(authenticate())
+        .put(task.toggleTask)
+
 }
