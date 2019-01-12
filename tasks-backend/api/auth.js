b3784235ad62736a693069c0f0e40e8a7004bfd5
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-01-12 19:24:54 -0400

    tasks-backend: corrigir e-mail case insensitive

--- tasks-backend/api/auth.js
+++ tasks-backend/api/auth.js
@@ -8,7 +8,7 @@ module.exports = app => {
             return res.status(400).send('Dados incompletos')
 
         const user = await app.db('users')
-            .where({ email: req.body.email })
+            .whereRaw("LOWER(email) = LOWER(?)", req.body.email)
             .first()
 
         if (user) {
