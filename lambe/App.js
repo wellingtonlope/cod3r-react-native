1624d9cf20369191a6d44b82afe1c179978f460d
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-01-13 12:09:58 -0400

    lambe: componente Post

--- lambe/App.js
+++ lambe/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import { View, Text } from 'react-native'
 import Header from './src/components/Header'
+import Post from './src/components/Post'
 
 export default class App extends Component {
     render() {
         return (
-            <Header />
+            <View style={{ flex: 1 }}>
+                <Header />
+                <Post image={require('./assets/imgs/fence.jpg')} />
+            </View>
         )
     }
 }
